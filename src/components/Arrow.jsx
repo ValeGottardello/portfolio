f607d5d1b13c8fdd7a3443936b7a3eefef77018b
ValeGottardello <valengottardello@hotmail.com>
2023-05-30 16:54:29 +1000

    delete this changes

---
+++ src/components/Arrow.jsx
@@ -0,0 +1,9 @@
+import { MdKeyboardDoubleArrowDown } from 'react-icons/md';
+
+export default function Arrow(props) {
+    return (
+        <a href={props.page}>
+            <MdKeyboardDoubleArrowDown/>
+        </a>      
+    )
+}
